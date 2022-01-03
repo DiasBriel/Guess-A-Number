904a1c67d13eb27aacedb4e07cead1f68160b504
Gabriel Dias <g.dias1@hotmail.com>
2022-01-03 12:33:58 -0300

    Result text styled

--- components/BodyText.js
+++ components/BodyText.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text } from "react-native";
 
-const BodyText = (props) => <Text style={styles.body}>{props.children}</Text>;
+const BodyText = (props) => (
+  <Text style={{ ...styles.body, ...props.style }}>{props.children}</Text>
+);
 const styles = StyleSheet.create({
   body: {
     fontFamily: "open-sans",
